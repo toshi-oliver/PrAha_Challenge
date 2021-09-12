505a602859de80a33b7217d19077e13bc8d6173a
Toshiaki Furukawa <furukawa@serverworks.co.jp>
2021-09-12 22:56:53 +0900

    環境構築

---
+++ kadai_38-44/sample/components/templates/first_page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export interface FirstPageProps {
+  text: string
+  number: number
+}
+
+// export const FirstPage = ({ text, number} : FirstPageProps) => {
+//   return (
+//     <p>{text}</p>
+//     <p>{ number }</p>
+    
+//   )
+// }
---
+++ kadai_38-44/sample/story/templates/first_page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export interface FirstPageProps {
+  text: string
+  number: number
+}
+
+export const FirstPage = (text, number : FirstPageProps) => {
+  return (
+    <p>{ text }</p>
+  )
+}
